1d2f3b5db5633d5b784ba4a697dbc0920df0130d
neha-madhini <40025677+neha-madhini@users.noreply.github.com>
2018-07-23 19:25:31 +0530

    Add files via upload

---
+++ Power.c
@@ -0,0 +1,57 @@
+int powmod(int x, int n, int d) 
+{
+    
+	long long s=1, t=x;
+        
+	if(x==0)
+	{
+        
+		return 0;
+    
+	}
+    
+	if(n==0)//pow is zero
+    
+	{
+        
+		return 1%d;
+    
+	}
+    
+	while(n>0)
+    
+	{
+        
+	if(n%2==1) //pow is odd
+        
+	{
+        
+		s=(s*t)%d;
+        
+		n--;
+    
+	}
+    
+	else //pow is even 
+    
+	{
+        
+		t=(t*t)%d;
+        
+		n=n/2;
+    
+	}
+    
+	}
+    
+	if(s<0)
+
+	{
+    
+		s=(s+d)%d;
+
+	}
+	
+return s;
+
+}
